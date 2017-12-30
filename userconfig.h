c2482c9d3c926c49e897c7bea487dad602c4e676
Sthopeless <hughbrian@hotmail.com>
2017-12-30 18:42:54 +0100

    Create userconfig.h

---
+++ userconfig.h
@@ -0,0 +1,14 @@
+// Wifi SSID
+#define mySSID "WIFI SSID HERE"
+
+// Wifi Passphrase
+#define myPASSWORD "WIFI PASSWORD HERE"
+
+// IP Address of the MQTT broker
+#define myMQTTBROKER "MQTT IP HERE"
+
+// MQTT broker username
+#define MQTTusername "MQTT USERNAME HERE"
+
+// MQTT broker password
+#define MQTTpassword "MQTT PASSWORD HERE"
